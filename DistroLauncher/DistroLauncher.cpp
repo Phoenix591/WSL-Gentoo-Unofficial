d844f2d2bbe3f010d833d9d0793b4a475ab899b8
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2020-01-15 06:18:27 -0600

    Prompt the user to create the root password, rather than hard coding one.

--- DistroLauncher/DistroLauncher.cpp
+++ DistroLauncher/DistroLauncher.cpp
@@ -36,14 +36,16 @@ HRESULT InstallDistribution(bool createUser)
         return hr;
     }
 
-    hr = g_wslApi.WslLaunchInteractive(L"/bin/echo root:gentoo | /usr/sbin/chpasswd && echo -e '\033[1;33mDefault root password set to \033[0;31mgentoo\033[1;33m !\033[0m'", true, &exitCode);
-    if (FAILED(hr)) {
-        return hr;
-    }
+
 
 
     // Create a user account.
     if (createUser) {
+
+        hr = g_wslApi.WslLaunchInteractive(L"/bin/echo 'Set the root password' && /usr/bin/passwd root", true, &exitCode);
+        if (FAILED(hr)) {
+            return hr;
+        }
         Helpers::PrintMessage(MSG_CREATE_USER_PROMPT);
         std::wstring userName;
         do {
